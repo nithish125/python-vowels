660fa30753664e81740b15e8285df9206e026cdf
nithish125 <nithishs3620@gmail.com>
2022-08-25 20:24:51 +0530

    add

---
+++ task1.py
@@ -0,0 +1,19 @@
+a="hi am fabev!34y"
+b=""
+d=''
+c=[]
+list1=["a","e"," ","i","o","u" ]
+for x in range(len(a)):
+    print( str(a[x]))
+    #if a[x] not in list1:
+#      v=a[x].isalpha
+#      print(v)
+#      if(v):
+#       c.append(a[x])
+#       b+=a[x]
+#     else:
+#         d+=a[x]
+# print(c)
+# print(b)
+# print(d)
+    
